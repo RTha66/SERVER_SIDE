8f803696c10d39dd740326bf78d85317eee9d647
RTha66 <139029980+RTha66@users.noreply.github.com>
2025-09-04 15:58:25 +0700

    gfg

---
+++ userRoutes.js
@@ -0,0 +1,10 @@
+import userController from "../controllers/userController.js"
+
+const useUserRoute = async (router) => {
+  router.get('/user', userController.getAllUsers)
+  router.get('/user/:id', userController.getUserById)
+  router.post('/user',userController.create)
+  router.post('/login',userController.login)
+}
+
+export default useUserRoute
